53b8be0be3d1f8cdf825760b60e49ff88cde57c5
Nick Arocho <nick@boldlyfine.com>
2018-02-25 22:28:24 -0800

    configure server/database

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+require('dotenv').config();
+require('./config/database');
+
+app.use(logger('dev'));
+// app.use(require('./config/auth'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// API routes
+// app.use('/api/users', require('./routes/api/users'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, function() {
+    console.log(`Express app running on port ${PORT}`)
+  });
