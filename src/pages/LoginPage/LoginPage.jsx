051bd5d1766f32025a0986525033bdd25ad394e5
Nick Arocho <nick@boldlyfine.com>
2018-02-28 16:53:14 -0800

    update login css

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,8 +4,8 @@ import './LoginPage.css';
 
 const LoginPage = (props) => {
   return (
-    <div className='LoginPage'>
-      <LoginForm 
+    <div className='LoginPage vertical-center flex-center-center'>
+      <LoginForm
         history={props.history}
         handleLogin={props.handleLogin}
       />
