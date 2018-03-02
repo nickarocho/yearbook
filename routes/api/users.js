0711afb6bc9af9cf70c0ac0b20e42d41336f0e7d
Nick Arocho <nick@boldlyfine.com>
2018-03-01 16:45:18 -0800

    add build

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../../models/user');
 var usersCtrl = require('../../controllers/users');
 
 /*---------- Public Routes ----------*/
