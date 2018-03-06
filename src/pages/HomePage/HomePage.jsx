dfbe7bbd20e8cba3e7751e4056c22db975629263
Nick Arocho <nick@boldlyfine.com>
2018-03-06 10:58:53 -0800

    add pic to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,11 +7,15 @@ import { Button } from 'react-materialize';
 const HomePage = (props) => {
 
   return (
-    <div className="HomePage">
-      <div className="vertical-center flex-center-center">
-        <Link to='/yearbook'>
-          <Button waves='light' className='#212121 grey darken-4'>View Yearbook</Button>
-        </Link>
+    <div className="Home-wrapper">
+      <div className="Home-page">
+        <div><h1>THS Class of '08</h1></div>
+        <div><Link className='View-btn' to='/yearbook'><img className="class-pic" src="https://i.imgur.com/Dpgcbyh.jpg" alt="class-of-2008" /></Link></div>
+        <div>
+          <Link className='View-btn' to='/yearbook'>
+            <Button waves='light' className='View-btn #263238 blue-grey darken-4'>View Yearbook</Button>
+          </Link>
+        </div>
       </div>
     </div>
   )
